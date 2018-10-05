bf7a735ce010cc792f8e14416f14bddcd3d11263
obivankanoobi <artium1@gmail.com>
2018-10-05 21:11:14 +0300

    some design change

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,10 @@ const Layout = (props) => (
     <div>
         <Head>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"/>
+            <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"
+          />
             <meta charSet="UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
--- pages/index.js
+++ pages/index.js
@@ -1,172 +1,213 @@
-import React , { Component } from 'react'
-import Layout from '../components/Layout.js'
-import Link from 'next/link'
-
-import axios from 'axios'
-import config from '../server/config.js'
+import React, { Component } from "react";
+import Layout from "../components/Layout.js";
+import Link from "next/link";
+import { Icon, Table, Input, Button } from "semantic-ui-react";
+import axios from "axios";
+import config from "../server/config.js";
 
 class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { }
-    }
-    
-    handleInput = (e) => {
-        let target = e.target;
-        let name = target.name;
-        let value = target.value;
-        this.setState({[name]:value})
-    }
+  handleInput = e => {
+    let target = e.target;
+    let name = target.name;
+    let value = target.value;
+    this.setState({ [name]: value });
+  };
 
-
-    takeScreenShot = (url, device) => {
-        console.log('screenshot')
-        this.setState({loading:true}, () => {
-            axios.get(`${config.server}/screenshot?url=${url}&device=${device}`)
-            .then((res)=> {
-                console.log(res.data)
-                if (res.data != 'no such page' && res.data != 'invalid url') {
-                    console.log("res.data != 'no such page' && res.data != 'invalid url'")
-                    this.setState({[device]:res.data.link}, () => {
-                        this.setState({
-                            showImage:true,
-                            loading:false,
-                            noPage:false,
-                        })
-                    })
-                } else if (res.data == 'no such page') {
-                    console.log(res.data)
-                    this.setState({
-                        noPage:true,
-                        loading:false,
-                        showImage:false,
-                        msg: `Sorry! can't find it`
-                    })
-                } else if (res.data == 'invalid url') {
-                    console.log(res.data)
-                    this.setState({
-                        noPage:true,
-                        loading:false,
-                        showImage:false,
-                        msg: `invalid address`
-                    })
-                }
-            })
-            .catch(e => console.log(e))
+  takeScreenShot = (url, device) => {
+    console.log("screenshot");
+    this.setState({ loading: true }, () => {
+      axios
+        .get(`${config.server}/screenshot?url=${url}&device=${device}`)
+        .then(res => {
+          console.log(res.data);
+          if (res.data != "no such page" && res.data != "invalid url") {
+            console.log(
+              "res.data != 'no such page' && res.data != 'invalid url'"
+            );
+            this.setState({ [device]: res.data.link }, () => {
+              this.setState({
+                showImage: true,
+                loading: false,
+                noPage: false
+              });
+            });
+          } else if (res.data == "no such page") {
+            console.log(res.data);
+            this.setState({
+              noPage: true,
+              loading: false,
+              showImage: false,
+              msg: `Sorry! can't find it`
+            });
+          } else if (res.data == "invalid url") {
+            console.log(res.data);
+            this.setState({
+              noPage: true,
+              loading: false,
+              showImage: false,
+              msg: `invalid address`
+            });
+          }
         })
+        .catch(e => console.log(e));
+    });
+  };
 
-    }
+  capture = url => {
+    this.takeScreenShot(url, "desktop");
+    this.takeScreenShot(url, "phone");
+  };
 
-    capture = (url) => {
-        this.takeScreenShot(url, 'desktop');
-        this.takeScreenShot(url, 'phone');
-    }
+  render() {
+    return (
+      <Layout>
+        <div className="text-center">
+          <h3 className="title align-center">
+            RESTful api to take a screenshot of any site{" "}
+            <span role="img" aria-label="camera">
+              📸
+            </span>
+          </h3>
+        </div>
+        <div className="align-center">
+          <div className="flex-container center test-it">
+            <br />
 
-    render() {
-        return(
-            <Layout>
-                <div className='text-center'>
-                    <h3 className='title align-center'>RESTful api to take a screenshot of any site <span role="img" aria-label="camera">📸</span></h3>
+            <Input
+              name="urlInput"
+              placeholder="Add link..."
+              type="text"
+              onChange={e => this.handleInput(e)}
+              action={
+                <Button
+                  loading={this.state.addingWebsiteLoader}
+                  color="teal"
+                  content="Capture"
+                  onClick={() => this.capture(this.state.urlInput)}
+                />
+              }
+              style={{ padding: "50px" }}
+            />
+          </div>
+          <br />
+          {!this.state.loading ? (
+            <div className="flex-container center">
+              {this.state.showImage ? (
+                <img className="dsk-img" src={this.state.desktop} />
+              ) : null}
+              {this.state.showImage ? (
+                <img className="phone-img" src={this.state.phone} />
+              ) : null}
+            </div>
+          ) : (
+            <p className="text-center">
+              one moment please
+              <span role="img" aria-label="thinking">
+                {" "}
+                💅
+              </span>
+              ...
+            </p>
+          )}
+          {!this.state.loading ? (
+            <div>
+              {!this.state.noPage ? (
+                <div className="text-center">
+                  <a
+                    target="_blank"
+                    className="block"
+                    href={this.state.desktop}
+                  >
+                    {this.state.desktop}
+                  </a>
+                  <a target="_blank" className="block" href={this.state.phone}>
+                    {this.state.phone}
+                  </a>
                 </div>
-                <div className='align-center'>
-                    <div className='flex-container center test-it'>
-                        <input onChange={this.handleInput} type='text' name='urlInput' className='url-input' placeholder='enter url'/>
-                        <br/>
-                        <button onClick={() => this.capture(this.state.urlInput)}>test it!!</button>
-                        <br/>
-                    </div>
-                    <br/>
-                    {
-                        !this.state.loading ?
-                        <div className='flex-container center'>
-                            {this.state.showImage ? <img className='dsk-img' src={this.state.desktop}/> : null}
-                            {this.state.showImage ? <img className='phone-img' src={this.state.phone}/> : null}
-
-                        </div> : <p className='text-center'>one moment please<span role="img" aria-label="thinking"> 💅</span>...</p>
-                    }
-                    {
-                        !this.state.loading ? 
-                            <div>
-                            {
-                                !this.state.noPage ? 
-                                    <div className='text-center'>
-                                        <a target='_blank' className='block' href={this.state.desktop}>{this.state.desktop}</a>
-                                        <a target='_blank' className='block' href={this.state.phone}>{this.state.phone}</a>
-                                    </div>  : <p className='text-center '>{this.state.msg}<span role="img" aria-label="thinking"> 🤷 </span></p>
-                            }
-                            </div> : null
-                    }
-
-                </div>
-                {style}
-            </Layout>
-        )
-    }
-} 
-
-Index.getInitialProps = async () => { // getInitialProps is a Next method to get props to be fetched to the page when its render
-    return {}
+              ) : (
+                <p className="text-center ">
+                  {this.state.msg}
+                  <span role="img" aria-label="thinking">
+                    {" "}
+                    🤷{" "}
+                  </span>
+                </p>
+              )}
+            </div>
+          ) : null}
+        </div>
+        {style}
+      </Layout>
+    );
+  }
 }
 
+Index.getInitialProps = async () => {
+  // getInitialProps is a Next method to get props to be fetched to the page when its render
+  return {};
+};
+
 const style = (
-    <style jsx="true">
-        {`
-            .align-center {
-                margin-left:auto;
-                margin-right:auto;
-                
-            }
+  <style jsx="true">
+    {`
+      .align-center {
+        margin-left: auto;
+        margin-right: auto;
+      }
 
-            .response-text {
-                position: absolute;
-                margin-left: auto;
-                margin-right: auto;
-                left: 0;
-                right: 0;
-                top:30%;
-            }
+      .response-text {
+        position: absolute;
+        margin-left: auto;
+        margin-right: auto;
+        left: 0;
+        right: 0;
+        top: 30%;
+      }
 
-            .block {
-                display:block;
-            }
+      .block {
+        display: block;
+      }
 
-            .title {
-                margin-bottom:5rem;
-                margin-top:0.5rem;
-            }
+      .title {
+        margin-bottom: 5rem;
+        margin-top: 0.5rem;
+      }
 
+      .flex-container {
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        display: flex;
+      }
 
-            .flex-container {
-                padding: 0;
-                margin: 0;
-                list-style: none;
-                display: flex;
-            }
+      .url-input {
+        width: 300px;
+      }
 
-            .url-input {
-                width:300px;
-            }
+      .center {
+        justify-content: center;
+      }
 
-            .center { 
-                justify-content: center; 
-            }
-            
-            .dsk-img {
-                width:auto;
-                height:200px;
-                margin-right:1rem;
-                margin-bottom:1rem;
-            }
+      .dsk-img {
+        width: auto;
+        height: 200px;
+        margin-right: 1rem;
+        margin-bottom: 1rem;
+      }
 
-            .phone-img {
-                width:auto;
-                height:200px;
-                margin-left:1rem;
-                margin-bottom:1rem;
-            }
-        `}
-    </style>
-)
+      .phone-img {
+        width: auto;
+        height: 200px;
+        margin-left: 1rem;
+        margin-bottom: 1rem;
+      }
+    `}
+  </style>
+);
 
-export default Index
+export default Index;
